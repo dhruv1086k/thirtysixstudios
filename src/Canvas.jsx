f93cc257b9b2266a733e10ae1bebc77749239ff3
Dhruv Pal <dhruvpal.4818@gmail.com>
2024-11-16 21:51:08 +0530

    add dynamic functioning to canvas using GSAP

--- src/Canvas.jsx
+++ src/Canvas.jsx
@@ -2,12 +2,24 @@ import { useState } from "react";
 import { useRef } from "react";
 import { useEffect } from "react";
 import canvasimages from "./canvasimages";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 export default function Canvas({ details }) {
   const canvasRef = useRef(null);
   const [index, setIndex] = useState({ value: details.startIndex });
 
-  
+  useGSAP(() => {
+    gsap.to(index, {
+      value: details.startIndex + 149,
+      duration: 3,
+      ease: "linear",
+      repeat: -1,
+      onUpdate: () => {
+        setIndex({value: Math.round(index.value)});
+      }
+    })
+  })
 
   useEffect(() => {
     const scale = window.devicePixelRatio;
